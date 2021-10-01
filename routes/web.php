3c21df7fda0012e75c80a9378a0d9c117e9ffa68
Roydano <roy.dano90@gmail.com>
2021-10-01 16:51:05 +0200

    add rotta login - admin

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Auth::routes();
 Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')
 ->group(function() {
 // pagina di atterraggio dopo il login (con il prefisso, l'url è '/admin')
-    Route::get('/', 'HomeController@index')->name('index');
+    Route::get('/', 'UserController@index')->name('index');
 }); 
 
