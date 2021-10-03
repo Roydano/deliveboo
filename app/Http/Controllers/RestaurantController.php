4ab24892d19ca48118277b51ab5f5f2aabfaec6d
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-10-04 00:09:15 +0200

    prova seed

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+
+use App\Restaurant;
+use App\Cuisine;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $restaurants = Restaurant::all;
+        return view ('index' , compact('restaurants'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //restaurant created during the registration: userController
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //see userController
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        $restaurant = Restaurant::where('slug', $slug)->first();
+        return view ('showRestaurant', compact('restaurant'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($slug)
+    {
+        $restaurant = Restaurant::where('slug', $slug)->first();
+        //add cuisine
+        return view ('admin.editRestaurant', compact('restaurant'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //sezione update restaurant: update info user in controller differente
+
+        $request->validate([
+            'restName' => 'required|string',
+            'address' => 'required|string'],
+            /* 'phone' => ['required', 'numeric', 'min:11'],
+            'p_iva' => ['required', 'numeric'], //add validation giusto
+            'img' => ['string'] */
+        ]);
+
+        $data = $request->all();
+
+        if($data['title'] != $post->title){
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //see userController
+    }
+}
