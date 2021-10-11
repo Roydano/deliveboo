1916dc6594860945f5d699aa4db6df17a2720525
davide <davide.baiocchi97@gmail.com>
2021-10-12 00:21:04 +0200

    Plate restaurant mod Grapich

--- resources/views/admin/editPlate.blade.php
+++ resources/views/admin/editPlate.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container text-center">
 
     <h2>Stai modificando il piatto</h2>
 
@@ -10,7 +10,7 @@
         @method('PUT')
 
         <div class="form-group">
-          <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="Nome piatto" value="{{ $plate->name }}">
+          <input type="text" class="form-control text-center  @error('name') is-invalid @enderror" id="name" name="name" placeholder="Nome piatto" value="{{ $plate->name }}">
           @error('name')
             <div class="alert alert-danger">{{ $message }}</div>
           @enderror
@@ -18,7 +18,7 @@
         
         <div class="form-group">
           <label for="courses">Seleziona una portata</label>
-          <select class="form-control" id="courses" name="course_id">
+          <select class="form-control text-center" id="courses" name="course_id">
               @foreach ($courses as $course)
                   <option value="{{$course->id}}" @if($course->id == old('course_id', $plate->course_id)) selected @endif>{{$course->name}}</option>
               @endforeach
@@ -26,22 +26,24 @@
         </div>
 
         <div class="form-group">
-          <textarea class="form-control @error('ingredients') is-invalid @enderror" id="ingredients" name="ingredients" rows="3" placeholder="Ingredienti">{{ $plate->ingredients }}</textarea>
+          <label for="courses">Scrivi gli ingredienti</label>
+          <textarea class="text-center form-control @error('ingredients') is-invalid @enderror" id="ingredients" name="ingredients" rows="3" placeholder="Ingredienti">{{ $plate->ingredients }}</textarea>
           @error('ingredients')
             <div class="alert alert-danger">{{ $message }}</div>
           @enderror
         </div>
 
         <div class="form-group">
-            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="3" placeholder="Descrizione">{{ $plate->description }}</textarea>
+          <label for="courses">Descrivi il piatto</label>
+            <textarea class="text-center form-control @error('description') is-invalid @enderror" id="description" name="description" rows="3" placeholder="Descrizione">{{ $plate->description }}</textarea>
             @error('description')
               <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
-        <div class="my-3">
+        <div class="mb-3">
           @if($plate->img)
-            <img src="{{ asset('storage/' . $plate->img) }}" alt="{{ $plate->name }}">
+            <img class="img-show-plate" src="{{ asset('storage/' . $plate->img) }}" alt="{{ $plate->name }}">
           @endif
         </div>
         <div class="mb-3">
