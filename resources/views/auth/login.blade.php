91914e3d17e3558e37d5980dba953920e191e47e
Luca Giozza <luca.giozza@gmail.com>
2021-10-12 10:40:07 +0200

    correzioni login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,184 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<!-- creazione pagina login -->
+
+
+
+<form method="POST" action="{{ route('login') }}">
+@csrf
+
+<h2>Login</h2>
+
+<!-- email -->
+
+  <label for="email"  >
+    <p class="label-txt ">{{ __('Indirizzo email') }}</p>
+    <input type="text" class="input mt-2  @error('email') is-invalid @enderror" name="email" id="name" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    @error('email')
+        <span class="invalid-feedback" role="alert">
+             <strong>{{ $message }}</strong>
+       </span>
+    @enderror
+    <div class="line-box mt-2">
+      <div class="line"></div>
+    </div>
+  </label>
+
+
+  <!-- password -->
+
+  <label for="password"  >
+    <p class="label-txt">{{ __('Password') }}</p>
+    <input  class="input mt-2" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" >
+    @error('password')
+       <span class="alert alert-danger" role="alert">
+         <strong>{{ $message }}</strong>
+       </span>
+    @enderror
+    <div class="line-box mt-2">
+      <div class="line " ></div>
+    </div>
+  </label>
+  
+ 
+  <!-- conferma password -->
+
+  <label for="password-confirm"  >
+    <p class="label-txt">{{ __('Conferma password') }}</p>
+    <input  class="input mt-2 " id="password-confirm" type="password" class="form-control " name="password_confirmation" required autocomplete="new-password" >
+  
+    <div class="line-box">
+      <div class="line "></div>
+    </div>
+  </label>
+
+  
+  <div>
+      <button type="submit">Login</button>
+  </div>
+</form>
+
+
+
+<style lang="scss" scoped>
+  
+
+body {
+  background: url('../img/lo-sfondo.jpg');
+  background-size:cover;
+}
+p{
+  margin-bottom:20px;
+}
+
+
+form {
+  width: 60%;
+  margin: 60px auto;
+  background: #efefef;
+  padding: 60px 120px 80px 120px;
+  text-align: center;
+  -webkit-box-shadow: 2px 2px 3px rgba(0,0,0,0.1);
+  box-shadow: 2px 2px 3px rgba(0,0,0,0.1);
+}
+label {
+  display: block;
+  position: relative;
+  margin: 40px 0px;
+}
+.label-txt {
+  position: absolute;
+  top: -1.6em;
+  padding: 10px;
+  font-family: sans-serif;
+  font-size: .8em;
+  letter-spacing: 1px;
+  color: rgb(120,120,120);
+  transition: ease .3s;
+}
+.input {
+  width: 100%;
+  padding: 10px;
+  background: transparent;
+  border: none;
+  outline: none;
+}
+.input-cus{
+    width: 3%;
+  padding: 10px;
+  background: transparent;
+  border: none;
+  outline: none;
+  margin-top:70px;
+}
+
+.line-box {
+  position: relative;
+  width: 100%;
+  height: 2px;
+  background: #BCBCBC;
+}
+
+.line {
+  position: absolute;
+  width: 0%;
+  height: 2px;
+  top: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+  background: #8BC34A;
+  transition: ease .6s;
+}
+
+.input:focus + .line-box .line {
+  width: 100%;
+}
+
+.label-active {
+  top: -3em;
+}
+
+button {
+  display: inline-block;
+  padding: 12px 24px;
+  background: rgb(220,220,220);
+  font-weight: bold;
+  color: rgb(120,120,120);
+  border: none;
+  outline: none;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: ease .3s;
+}
+
+button:hover {
+  background: #8BC34A;
+  color: #ffffff;
+}
+
+.cucine{
+    background: #8BC34A;
+    opacity:0.8;
+}
+
+h2{
+    color:#bcbcbc;
+    margin-right:50px;
+}
+
+
+</style>
+
+
+@endsection
+
+
+
+
+<!-- pagina login con bootstrap -->
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +246,11 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</div> -->
+
+
+
+
+
+
+
